56338da7afbbeb68b71daa24889195e37bc4940c
15029 <cameronweiss021@gmail.com>
2026-01-15 09:23:52 -0500

    Php day 2

---
+++ contactAction.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+    <title>Contact Us Action</title>
+</head>
+<body>
+<h1>See your information below:</h1>
+
+Name: <?php echo $_GET["name"]; ?> <br />
+Email: <?php echo $_GET["email"]; ?> <br />
+Message: <?php echo $_GET["message"]; ?> <br />
+<?
+
+$myFirstVariable = 5;
+$anotherOne = "djKhalid";
+
+$num1 = 1;
+$num2 = 3.14159265358979323;
+
+//if else elif
+if($num1 > $num2){
+    echo "num1 is bigger!";
+
+}else{
+    echo "num2 is bigger";
+}
+
+?>
+</body>
+</html>
--- index.php
+++ index.php
@@ -3,10 +3,7 @@
     <title>Welcome to the App!</title>
 </head>
 <body>
-Hello World!!!
-<?php
-phpinfo();
-
-?>
+<h1>Welcome to The App App</h1>
+<a href="contact.php">Contact Us</a>
 </body>
 </html>
